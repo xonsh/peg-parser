47de2322fc210a1cd4640a9bd8205f2c8a47fae2
Noortheen Raja <jnoortheen@gmail.com>
2023-04-06 17:12:11 +0530

    refactor: update sample usage

--- tests/sample.py
+++ tests/sample.py
@@ -1,14 +1,25 @@
 # for testing with monkeytype run
 
-from xonsh_parser.parser import get_parser_cls
 
-parser = get_parser_cls()()
+def main():
+    from xonsh_parser.parser import get_parser_cls
 
-# cmd line
-parser.parse("ls -alh")
+    parser = get_parser_cls()()
 
-try:
-    # invalid syntax
-    parser.parse("print(1")
-except Exception:
-    pass
+    # cmd line
+    parser.parse("ls -alh")
+
+    try:
+        # invalid syntax
+        parser.parse("print(1")
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    # main()
+    from pathlib import Path
+
+    from xonsh_parser.parser import write_parser_table
+
+    write_parser_table(output_path=Path("/tmp/bytes-2.pickle"))
