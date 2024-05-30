d47e6d73758c9c25a7fa9054c7f82b209708ccd3
Noortheen Raja <jnoortheen@gmail.com>
2024-05-30 13:57:40 +0530

    test: add large file benchmarking

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,21 +1,36 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+from pathlib import Path
 
-def parse(code: str):
+small_code = "![ls -alh]"
+file = Path(__file__).parent.parent / "peg_parser" / "parser.py"
+
+def parse(code: str | Path):
     from peg_parser.parser import XonshParser
-    return XonshParser.parse_string(code, mode="exec")
+    if isinstance(code, str):
+        return XonshParser.parse_string(code, mode="exec")
+    return XonshParser.parse_file(code)
 
 
 class TimeSuite:
-    def time_parser_init(self):
-        parse("![ls -alh]")
+    def time_parse_small_string(self):
+        parse(small_code)
+
+    def time_large_files(self):
+        parse(file)
 
 
 class MemSuite:
-    def mem_parser_init(self):
-        parse("![ls -alh]")
+    def mem_parse_small(self):
+        parse(small_code)
+
+    def mem_parser_large_file(self):
+        parse(file)
 
 class PeakMemSuite:
-    def peakmem_parser_init_(self):
+    def peakmem_parse_small(self):
         parse("![ls -alh]")
+
+    def peakmem_parser_large_file(self):
+        parse(file)
