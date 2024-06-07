16eb91c406d353b14a7681e39560dffd4a300a73
Noortheen Raja <jnoortheen@gmail.com>
2024-06-07 22:13:48 +0530

    feat: implement Token Enum

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@
 
 from __future__ import annotations
 
+import maturin_import_hook
+
+maturin_import_hook.install()
+
 import ast
 import contextlib
 import io
--- xonsh_tokenizer/src/tokenizer.rs
+++ xonsh_tokenizer/src/tokenizer.rs
@@ -1,4 +1,18 @@
-enum Token {
+use pyo3::prelude::*;
+
+use std::hash::Hasher;
+use std::hash::{DefaultHasher, Hash};
+
+// import the macros needed
+use strum_macros;
+
+#[pyclass(
+    frozen,
+    rename_all = "SCREAMING_SNAKE_CASE",
+    module = "xonsh_tokenizer"
+)]
+#[derive(Debug, Hash, PartialEq, Eq, strum_macros::Display)]
+pub enum Token {
     ENDMARKER,
     NAME,
     NUMBER,
@@ -9,18 +23,38 @@ enum Token {
     OP,
     AWAIT,
     ASYNC,
-    TYPE_IGNORE,
-    TYPE_COMMENT,
-    SOFT_KEYWORD,
-    FSTRING_START,
-    FSTRING_MIDDLE,
-    FSTRING_END,
+
+    TypeIgnore,
+    TypeComment,
+    SoftKeyword,
+    FstringStart,
+    FstringMiddle,
+    FstringEnd,
     ERRORTOKEN,
     COMMENT,
     NL,
     ENCODING,
     // xonsh specific tokens
-    SEARCH_PATH,
-    MACRO_PARAM,
+    SearchPath,
+    MacroParam,
     WS,
+}
+
+#[pymethods]
+impl Token {
+    fn is_ws(&self) -> bool {
+        match self {
+            Token::ENDMARKER => true,
+            Token::NEWLINE => true,
+            Token::INDENT => true,
+            Token::DEDENT => true,
+            _ => false,
+        }
+    }
+
+    fn __hash__(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.hash(&mut hasher);
+        hasher.finish()
+    }
 }
