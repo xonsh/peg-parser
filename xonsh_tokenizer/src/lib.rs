57c07643b8e91bd29620b0e500e57aee004670e8
Noortheen Raja <jnoortheen@gmail.com>
2024-06-11 02:53:34 +0530

    feat: create pyo3 bridge

--- xonsh_tokenizer/src/lib.rs
+++ xonsh_tokenizer/src/lib.rs
@@ -1,18 +1,52 @@
-pub mod tokenizer;
 mod regex;
+pub mod tokenizer;
 
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
+use tokenizer::{tokenize_string, TokInfo};
+
+#[pyclass(frozen)]
+#[derive(Clone)]
+pub struct PyTokInfo {
+    pub inner: tokenizer::TokInfo,
+}
+
+#[pymethods]
+impl PyTokInfo {
+    fn __repr__<'a>(&'a self) -> PyResult<String> {
+        Ok(format!("Tok<{:?}:'{}'>", self.inner.typ, self.inner.string))
+    }
+
+    fn __getattr__<'py>(slf: PyRef<'py, Self>, py: Python<'py>, name: &str) -> PyResult<PyObject> {
+        let obj = match name {
+            "type" => format!("{:?}", slf.inner.typ).into_py(py),
+            "string" => slf.inner.string.clone().into_py(py),
+            "start" => slf.inner.start.clone().into_py(py),
+            "end" => slf.inner.end.clone().into_py(py),
+            _ => return Err(PyTypeError::new_err(format!("Unknown attribute: {}", name))),
+        };
+        Ok(obj)
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn tokenize_str(src: &str) -> PyResult<Vec<PyTokInfo>> {
+    let mut tokens = Vec::new();
+    for token in tokenize_string(src) {
+        if let Ok(inner) = token {
+            tokens.push(PyTokInfo{inner});
+        } else {
+            return Err(PyTypeError::new_err(token.unwrap_err()));
+        }
+    }
+    Ok(tokens)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn xonsh_tokenizer(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    // m.add_class::<tokenizer::Token>()?;
+    m.add_function(wrap_pyfunction!(tokenize_str, m)?)?;
+    m.add_class::<PyTokInfo>()?;
     Ok(())
 }
