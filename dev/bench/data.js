2fd611a556fd58e8f8877086d80e170bdbe75c20
github-action-benchmark <github@users.noreply.github.com>
2024-08-04 11:25:07 +0000

    add Python Benchmark (pytest) benchmark result for d98651b8a9cd527299739963179c63617ec24591

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722737162359,
+  "lastUpdate": 1722770707535,
   "repoUrl": "https://github.com/xonsh/peg-parser",
   "entries": {
     "Python Benchmark": [
@@ -38,6 +38,58 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.041867817101583986",
             "extra": "mean: 3.5337677749999954 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jnoortheen@gmail.com",
+            "name": "Noortheen Raja",
+            "username": "jnoortheen"
+          },
+          "committer": {
+            "email": "jnoortheen@gmail.com",
+            "name": "Noortheen Raja",
+            "username": "jnoortheen"
+          },
+          "distinct": true,
+          "id": "d98651b8a9cd527299739963179c63617ec24591",
+          "message": "feat: add ply to benchmarks",
+          "timestamp": "2024-08-04T16:51:07+05:30",
+          "tree_id": "241251c04835e0e4ada8e2ef6699f06d36738c0e",
+          "url": "https://github.com/xonsh/peg-parser/commit/d98651b8a9cd527299739963179c63617ec24591"
+        },
+        "date": 1722770706722,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_bench.py::TestBenchSmallString::test_peg",
+            "value": 1699.8403430711694,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000042747789210107655",
+            "extra": "mean: 588.2905439185306 usec\nrounds: 296"
+          },
+          {
+            "name": "tests/test_bench.py::TestBenchSmallString::test_ply",
+            "value": 4565.591520228812,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000024358606487752092",
+            "extra": "mean: 219.02966911719759 usec\nrounds: 272"
+          },
+          {
+            "name": "tests/test_bench.py::TestBenchLargeFile::test_pegen",
+            "value": 0.2807886967024433,
+            "unit": "iter/sec",
+            "range": "stddev: 0.037457739869209784",
+            "extra": "mean: 3.561396921400001 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_bench.py::TestBenchLargeFile::test_ply",
+            "value": 0.8259526905716784,
+            "unit": "iter/sec",
+            "range": "stddev: 0.003495319902386961",
+            "extra": "mean: 1.2107230976000039 sec\nrounds: 5"
           }
         ]
       }
