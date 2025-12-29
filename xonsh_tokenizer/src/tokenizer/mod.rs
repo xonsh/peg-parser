a62137aa6245b09f003769f6660e95f75e13be45
Noortheen Raja <jnoortheen@gmail.com>
2025-12-29 14:55:27 +0530

    refactor: remove xonsh-tokenizer for winnow based one

--- xonsh_tokenizer/src/tokenizer/mod.rs
+++
@@ -1,6 +0,0 @@
-mod end_prog;
-mod line_state;
-pub mod main;
-mod state;
-pub mod tok;
-mod result;
--- xonsh_tokenizer/src/tokenizer/result.rs
+++
@@ -1,8 +0,0 @@
-use crate::tokenizer::tok::TokInfo;
-
-pub(crate) enum LoopAction {
-    Break,
-    None,
-}
-
-pub(crate) type LoopResult = (LoopAction, Vec<TokInfo>);
